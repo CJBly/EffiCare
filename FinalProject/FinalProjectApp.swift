eec6d95a432a519c7062c08138b4132a4350da53
CJBLY <cjbly@asu.edu>
2025-10-30 14:56:17 -0700

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Connor Bly on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
