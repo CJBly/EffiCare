b6c216f207dfe91e58cb19ae7582bb2866b0f1a1
CJBLY <cjbly@asu.edu>
2026-03-30 17:34:37 -0700

    Patient Conditions Views for Patient Users

---
+++ FinalProject/ConditionsView.swift
@@ -0,0 +1,42 @@
+//
+//  ConditionsView.swift
+//  FinalProject
+//
+//  Created by Connor Bly on 11/22/25.
+//
+import SwiftUI
+import SwiftData
+struct ConditionsView: View{
+    @ObservedObject var patientViewModel: PatientViewModel
+    @State private var showAddCondition = false
+    @Environment(\.modelContext) private var context
+    
+    var body: some View{
+        NavigationView{
+            List{
+                ForEach(patientViewModel.conditions, id: \.self){ condition in
+                    NavigationLink(destination: ConditionDetailsView(condition: condition, currentUser: patientViewModel.currentUser, context: context)){
+                        VStack(alignment: .leading){
+                            Text(condition.name)
+                                .font(.headline)
+                            Text("Severity: \(Int(condition.severity))")
+                                .font(.subheadline)
+                        }
+                    }
+                }
+                .onDelete(perform: patientViewModel.deleteCondition)
+            }
+            .navigationTitle("Conditions")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button(action: {showAddCondition = true}){
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+            .sheet(isPresented: $showAddCondition){
+                AddConditionView(patientViewModel: patientViewModel)
+            }
+        }
+    }
+}
